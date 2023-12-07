1da303c7e78c192606255b671c35f806f9925c7c
DragonflyStats <kobriendublin@gmail.com>
2023-12-07 11:02:34 +0000

    reload

--- ProbCurves.R
+++
@@ -1,8 +0,0 @@
-
-#Plotting the probability density function and the cumulative distribution 
-#function of the standard normal distribution.
-
-z=seq(-3,3,by=0.01)
-par(mfrow=c(2, 1))
-plot(z,dnorm(z),type='l',col="red",lwd=4)
-plot(z,pnorm(z),type='l',col="red",lwd=4)
